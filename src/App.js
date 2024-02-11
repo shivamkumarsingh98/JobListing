395bddeadc95dc227d240f7bb5ad9ba77420da68
shivamkumarsingh98 <shiavmkumarsingh.chu98@gmail.com>
2024-02-11 23:31:48 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import React from 'react';
 import Register from './Component/Register';
-import { Navigate,Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import Home from './Page/Home';
 import Login from './Component/Login'
 import Jobcreat from './Component/Jobcreat';
@@ -13,18 +13,14 @@ import Jobdetails from './Card/Jobdetails';
 function App() {
   return (
     <div >
-        <Routes>
+      <Routes>
         <Route path="/*" element={<Navigate to="/Home" />} />
-        <Route path="/Home" element={< Home/>} />
+        <Route path="/Home" element={< Home />} />
         <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register/>} />
-          <Route path='/Jobcreat' element={<Jobcreat/>}/>
-          <Route path='/Jobdetails' element={<Jobdetails/>}/>
-         
-          
-        </Routes>
-     
-      
+        <Route path="/Register" element={<Register />} />
+        <Route path='/Jobcreat' element={<Jobcreat />} />
+        <Route path='/Jobdetails' element={<Jobdetails />} />
+      </Routes>
     </div>
   );
 }
